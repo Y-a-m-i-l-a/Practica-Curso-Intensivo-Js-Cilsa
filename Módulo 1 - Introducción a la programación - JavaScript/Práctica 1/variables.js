541023ad5b8528ad7b2b8987153222fce17c00d0
Yamila C. Bruzzone <yamilabruzzone@hotmail.com>
2022-03-15 16:52:51 -0300

    Creando repositorio

---
+++ Módulo 1 - Introducción a la programación - JavaScript/Práctica 1/variables.js
@@ -0,0 +1,7 @@
+var nombre = prompt('¿Cuál es tu nombre?');
+var apellido = prompt('¿Cuál es tu apellido?');
+var edad = prompt('¿Cuál es tu edad?');
+
+document.getElementById("nombre").innerHTML = nombre
+document.getElementById("apellido").innerHTML = apellido
+document.getElementById("edad").innerHTML = edad
---
+++ Módulo 1 - Introducción a la programación - JavaScript/Práctica 2/nuevocodigo.js
@@ -0,0 +1,10 @@
+// Se imprimirá por consola el mensaje: "¡Hola Mundo! Este es mi primer Script"
+console.log("¡Hola Mundo! Este es mi primer Script");
+
+// En el elemento con ID "script" se insertará eñ string: "Ejemplos de cómo incluir JavaScript en un documento HTML"
+document.getElementById("script").innerHTML =
+    "Ejemplos de cómo incluir JavaScript en un documento HTML";
+
+/* Antes de que cargue el HTML aparecerá una alerta con el mensaje: "Ver lo simple que es agregar "comillas dobles" y ‘comillas simples’."
+Una vez que el usuario acepte la alerta, aparecerá el resto del HTML cargado.*/
+alert('Ver lo simple que es agregar "comillas dobles" y ‘comillas simples’.');
